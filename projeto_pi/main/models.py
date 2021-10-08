b5516eb302361e3710c694f18c296f3d774b4d82
João Pedro <jpedro.santana@outlook.com>
2021-10-08 20:43:34 -0300

    parametrizacao do ambiente e criacao da tabela

---
+++ projeto_pi/main/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+# Criação da(s) tabela(s)
+class Empreendimento(models.Model):
+    cadastro = models.AutoField(primary_key=True)
+    nome_empreendedor = models.CharField(max_length=20)
+    nome_empreendimento = models.CharField(max_length=30)
+    telefone = models.CharField(max_length=11, blank=True)
+    instagram = models.CharField(max_length=30, blank=True)
+    facebook = models.CharField(max_length=30, blank=True)
+    descricao = models.TextField(max_length=200)
+    imagem = models.ImageField(upload_to='imagens')
+
+    class Meta:
+        ordering=('nome_empreendimento',) #faz o ordenamento por empreendimento
+
+    def __str__(self):
+        return self.nome_empreendimento

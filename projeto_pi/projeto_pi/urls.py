fad83a0917e77e7383f6daeeec5b02481b31b760
João Pedro <jpedro.santana@outlook.com>
2021-10-09 16:42:00 -0300

    mapeamento de url para paginas de ajuda e anuncie

--- projeto_pi/projeto_pi/urls.py
+++ projeto_pi/projeto_pi/urls.py
@@ -19,5 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ajuda/', TemplateView.as_view(template_name='ajuda.html'), name='ajuda'),
+    path('anuncie/', TemplateView.as_view(template_name='anuncie.html'), name='anuncie'),
     path('', TemplateView.as_view(template_name='index.html'), name='index'), #redireciona qualquer requisição base para index.html
 ]

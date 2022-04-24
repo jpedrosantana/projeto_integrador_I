6c1c83846507e11a2f904f41f215d5a5989854cd
João Pedro <jpedro.santana@outlook.com>
2022-04-24 18:52:42 -0300

    correcao na funcao de redirect

--- projeto_pi/static/js/link-redirect.js
+++ projeto_pi/static/js/link-redirect.js
@@ -1,7 +1,7 @@
 function insta_link(){
     var insta_href = document.getElementById("instagram")
     var insta = document.getElementById("instagram").href;
-    var insta_alterado = insta.search(insta.slice("@")+1);
+    var insta_alterado = insta.slice(insta.search("@")+1);
 
     var url = "https://instagram.com/"+insta_alterado;
     insta_href.setAttribute("href", url)

eac9095f36289deae5c09346427dd44b31b3802c
João Pedro <jpedro.santana@outlook.com>
2022-04-24 18:36:36 -0300

    alterado script para redirecionar link de instagram em qualquer url

--- projeto_pi/static/js/link-redirect.js
+++ projeto_pi/static/js/link-redirect.js
@@ -1,7 +1,7 @@
 function insta_link(){
     var insta_href = document.getElementById("instagram")
-    var insta = document.getElementById("instagram");
-    var insta_alterado = insta.href.slice(38);
+    var insta = document.getElementById("instagram").href;
+    var insta_alterado = insta.search(insta.slice("@")+1);
 
     var url = "https://instagram.com/"+insta_alterado;
     insta_href.setAttribute("href", url)
